bda78fd76ff51d782cc6f159f8afc43787b3a9aa
Your Name <drjpolanco25@yahoo.com>
2022-09-21 17:15:39 -0700

    runit

--- server/index.js
+++ server/index.js
@@ -16,6 +16,14 @@ app.use(cors());
 app.use('/posts', postRoutes);
 app.use("/user", userRouter);
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
 const CONNECTION_URL = 'mongodb+srv://mikeberg30:mikeberg30@cluster0.sc2qkp6.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 5000;
 
